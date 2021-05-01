ed917dd64bbfd65ecb6040a292f5e12f44a5aa39
Lucas Santos <leos9877@gmail.com>
2021-05-01 19:03:06 -0300

    implementando projeto

--- apps/cadastro/forms.py
+++ apps/cadastro/forms.py
@@ -3,7 +3,15 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'subtitle', 'text')
+        
+        error_messages = {
+            "title":{
+                "required": "Title is required to complete the post.",
+            },
+            "text":{
+                "required": "It is mandatory to fill in the text to complete the post."
+            },
+        }
--- apps/cadastro/models.py
+++ apps/cadastro/models.py
@@ -3,12 +3,32 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        verbose_name="Author",
+    )
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Title",
+    )
+    subtitle = models.CharField(
+        max_length=200,
+        null=True,
+        verbose_name="SubTitle",
+    )
+    text = models.TextField(
+        verbose_name="Text",
+    )
+    created_date = models.DateTimeField(
+        default=timezone.now,
+        verbose_name="Created Date",
+    )
+    published_date = models.DateTimeField(
+        blank=True, 
+        null=True,
+        verbose_name="Published Date",
+    )
 
     def publish(self):
         self.published_date = timezone.now()
